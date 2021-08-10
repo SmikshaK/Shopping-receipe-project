d32f6e7d677511e795065825bc828e4cc9831f3c
Smiksha <smiksha.kundan@zs.com>
2021-08-10 16:42:35 +0530

    Added static recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit 
+{  
+  recipes: Recipe[] = [
+  new Recipe('A test recipe', 'This is simply a test', 'https://www.thespruceeats.com/thmb/Y444Z8OGcofBJJ7MLFCrwDjcMEI=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-and-easy-chocolate-cake-recipe-995137-hero-01-e361ee51a67d4c12afb451cc48fb1940.jpg)')  
+];
 
   constructor() { }
 
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,15 @@
+import { StringMapWithRename } from '@angular/compiler/src/compiler_facade_interface';
+
+export class Recipe
+{
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string)
+    {
+        this.name= name;
+        this.description= desc;
+        this.imagePath= imagePath;
+    }
+}
